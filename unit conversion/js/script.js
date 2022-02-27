ba89c0c43262aa8c653f12b96a56f55cc24ccc31
MIlan991 <81048118+MIlan991@users.noreply.github.com>
2022-02-27 19:11:03 +0400

    Add files via upload

--- unit conversion/js/script.js
+++ unit conversion/js/script.js
@@ -7,19 +7,31 @@ let massEl = document.getElementById('mass-id');
 
 function length() {
     let metersCalc = valueEl.value * 3.2808399;
+    let metersCalcN = metersCalc.toFixed(3);
+
     let feetCalc = valueEl.value * 0.3048;
-    lengthEl.innerHTML = valueEl.value + " meters = " + metersCalc + " feet | " + valueEl.value + " feet = " + feetCalc;
+    let feetCalcN = feetCalc.toFixed(3);
+
+    lengthEl.innerHTML = valueEl.value + " meters = " + metersCalcN + " feet | " + valueEl.value + " feet = " + feetCalcN + " meters";
 }
 
 
 function volume() {
     let litersCalc = valueEl.value * 0.264172052;
+    let litersCalcN = litersCalc.toFixed(3);
+
     let gallonsCalc = valueEl.value * 3.78541178;
-    volumeEl.innerHTML = valueEl.value + " liters = " + litersCalc + " gallons | " + valueEl.value + " gallons = " + gallonsCalc + " liters";
+    let gallonsCalcN = gallonsCalc.toFixed(3);
+
+    volumeEl.innerHTML = valueEl.value + " liters = " + litersCalcN + " gallons | " + valueEl.value + " gallons = " + gallonsCalcN + " liters";
 }
 
 function mass() {
     let poundsCalc = valueEl.value * 2.20462262;
+    let poundsCalcN = poundsCalc.toFixed(3);
+
     let kilosCalc = valueEl.value * 0.45359237;
-    massEl.innerHTML = valueEl.value + " kilos = " + poundsCalc + " pounds | " + valueEl.value + " pounds = " + kilosCalc + " kilos";
+    let kilosCalcN = kilosCalc.toFixed(3);
+
+    massEl.innerHTML = valueEl.value + " kilos = " + poundsCalcN + " pounds | " + valueEl.value + " pounds = " + kilosCalcN + " kilos";
 }
